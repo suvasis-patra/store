30e35b28a6282034d255b7e0d671d20ab751ec24
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2024-12-04 10:47:55 +0530

    add register functionality

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,28 @@
+import { NextFunction,Request,Response } from "express";
+import { asyncHandler } from "../utils/asyncHandler";
+import { UserRegistrationSchema } from "../utils/validation";
+import { ApiError } from "../utils/ApiError";
+import { ErrorCode } from "../constants";
+import { PrismaClient } from "@prisma/client";
+import { ApiResponse } from "../utils/ApiResponse";
+
+const prisma = new PrismaClient();
+
+export const registerUser=asyncHandler(async(req:Request,res:Response,next:NextFunction)=>{
+      const validatedFields=UserRegistrationSchema.safeParse(req.body);
+      if(!validatedFields.success){
+            throw new ApiError(400,"Failed validation!",ErrorCode.VALIDATION_FAILED)
+      }
+      const {email,fullName,phNumber}=validatedFields.data;
+      const user = await prisma.user.create({
+            data:{
+                  email,
+                  name:fullName,
+                  phNumber
+            }
+      })
+      if(!user){
+            throw new ApiError(500,"Failed to register!",ErrorCode.DATABASE_ERROR);
+      }
+      res.status(201).json(new ApiResponse(201,{userId:user.id},"success!"))
+})
