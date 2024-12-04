58f0fae71af5386ce2ea409df6d97068c830589c
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2024-12-04 10:27:13 +0530

    add page not found error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
-
+import userRouter from "./routes/user.controller"
 
 const app=express();
+
+app.use(express.json());
+
+
+app.use("/api/v1/user",userRouter)
 
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world!")
+import dotenv from "dotenv";
+import {app} from "./app";
+
+dotenv.config();
+
+
+app.listen(process.env.PORT||8080,()=>console.log(`server is running on port ${process.env.PORT}`))
