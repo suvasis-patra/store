970b7bf0e80497d8ee550d0ec2d306c70344a69d
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2024-12-05 16:55:14 +0530

    update schema relation action

---
+++ prisma/migrations/20241205112438_add_relation_action/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_buyerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_productId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_buyerId_fkey" FOREIGN KEY ("buyerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
