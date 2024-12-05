87b0f5ef5ca221cea012c51dd6bdef81386292b2
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2024-12-05 16:47:26 +0530

    add get user by product route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getOrdersByUser, getUserById, registerUser, updateUser } from "../controllers/user.controller";
+import { getOrdersByUser, getUserById, getUserByProduct, registerUser, updateUser } from "../controllers/user.controller";
 
 
 const router =Router();
@@ -9,6 +9,7 @@ router.route("/register").post(registerUser);
 router.route("/get-user/:userId").get(getUserById);
 router.route("/get-user-order/:userId").get(getOrdersByUser)
 router.route("/update-user/:userId").patch(updateUser);
+router.route("/get-user-by-product/:productId").get(getUserByProduct)
 
 
 export default router;
