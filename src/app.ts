133a6d3ac63022a3e6ab4ed265e256227a9ca129
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2024-12-05 20:19:40 +0530

    update health check end point

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,13 @@ import { notFoundErrorHandler } from "./controllers/error.controller";
 import { errorHandler } from "./middlewares/error.middleware";
 import productRouter from "./routes/product.route"
 import orderRouter from "./routes/order.route"
+import { testEndPoint } from "./controllers/test.controller";
 
 const app=express();
 
 app.use(express.json());
 
-
+app.get("/health",testEndPoint);
 app.use("/api/v1/user",userRouter)
 app.use("/api/v1/product",productRouter)
 app.use("/api/v1/order",orderRouter)
